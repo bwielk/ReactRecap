c97d47a83f682987be730450b0388e5ba5d3ba41
bwielk <bwielk@gmail.com>
2019-02-17 23:12:12 +0000

    Adding props to the component

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Welcome to the app</h1>
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
+        <Person friendName="Alice" hobby="drawing"></Person>
+        <Person friendName="Stephanie" hobby="playing the guitar"></Person>
+        <Person friendName="Arthur" hobby="gardening"></Person>
       </div>
       // The equivalent of the code written above
       //React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'This works now'), 'I am a react App')
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const person = () => {
-  return <p>I am a person. How are you doing?</p>
+const person = (props) => {
+  const array = ["Alex", "Brian", "Cindy"]
+  return <p>I am a person.
+          I am {Math.floor(Math.random()*30)} years old!
+          My name is {array[Math.floor(Math.random()*3)]}.
+          How are you doing?
+          My best friend's name is {props.friendName} and I am so much into {props.hobby}
+        </p>
 };
 
 export default person;
