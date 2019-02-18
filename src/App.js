641f1c54224171f02de0a36a02620ba6a4cfef60
bwielk <bwielk@gmail.com>
2019-02-18 19:10:45 +0000

    Added an active button

--- src/App.js
+++ src/App.js
@@ -4,13 +4,29 @@ import './App.css';
 import Person from './Person/Person.js'
 
 class App extends Component {
+  state = {
+    number_of_clicks: 1,
+    persons_names: [
+      {name: "Max", age: 29},
+      {name: "Alex", age: 34},
+      {name: "Bobby", age: 21}
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log("Clicked")
+    alert('Clicked ' + this.state.number_of_clicks + ' times')
+    this.state.number_of_clicks += 1
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Welcome to the app</h1>
-        <Person friendName="Alice" hobby="drawing"></Person>
-        <Person friendName="Stephanie" hobby="playing the guitar"></Person>
-        <Person friendName="Arthur" hobby="gardening"></Person>
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person name={this.state.persons_names[0].name} hobby="drawing"></Person>
+        <Person name={this.state.persons_names[1].name} hobby="playing the guitar"></Person>
+        <Person name={this.state.persons_names[2].name} hobby="gardening"></Person>
       </div>
       // The equivalent of the code written above
       //React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'This works now'), 'I am a react App')
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 const person = (props) => {
   const array = ["Alex", "Brian", "Cindy"]
-  return <p>I am a person.
-          I am {Math.floor(Math.random()*30)} years old!
-          My name is {array[Math.floor(Math.random()*3)]}.
-          How are you doing?
-          My best friend's name is {props.friendName} and I am so much into {props.hobby}
-        </p>
+  return (
+    <div>
+          <hr/>
+          <p>I am a person</p>
+          <p>My name is {props.name}</p>
+          <p>How are you doing?</p>
+          <p>My best friend's name is {array[Math.floor(Math.random()*3)]} and I am so much into {props.hobby}</p>
+    </div>
+  );
 };
 
 export default person;
